0a0e1d8810ae410f304df9a7bac8bf328ea5e5a4
Levast David <dasyx@MacBook-Pro-de-Levast.local>
2023-11-05 22:15:39 +0100

    add menu + prompt + attempt counter

--- index.js
+++ index.js
@@ -1,9 +1,47 @@
-import { prompt } from './prompt.js';
+import { prompt } from "./prompt.js";
 
-// 🦁 Ici, je peux utiliser prompt comme si c'était une fonction normale
-// 🦁 Je peux stocker le résultat dans une variable
+console.log(` 
+🦁 Welcome to the Number Guessing Game! 🎮
 
-const targetNumber = () => Math.floor(Math.random() * 100);
-console.log(targetNumber());
+Rules:
+1. The system will generate a random number between 0 and 100.
+2. Your task is to guess this number.
+3. Enter a number when prompted.
+4. If your guess is too high or too low, you'll be notified, and you can guess again.
+5. The game continues until you guess the correct number.
 
-const guessNumber = prompt('Guess a number between 1 and 100:');
+Let's get started! 🚀
+
+`);
+
+const targetNumber = Math.floor(Math.random() * 101);
+console.log(targetNumber);
+
+const guessNumberGame = (attempt = 1) => {
+  if (attempt > 10) {
+    console.log("You've reached the maximum number of attempts. Game over.");
+    return;
+  }
+  if (attempt > 1) {
+    console.log(`You have ${11 - attempt} attempts left.`);
+  }
+
+  const guessNumber = Number(prompt("Guess a number between 0 and 100 : "));
+
+  if (guessNumber < 0 || guessNumber > 100 || isNaN(guessNumber)) {
+    console.log("Please enter a number between 0 and 100.");
+    return guessNumberGame(attempt);
+  }
+
+  if (guessNumber > targetNumber) {
+    console.log("Your guess is too high!");
+    return guessNumberGame(attempt + 1);
+  } else if (guessNumber < targetNumber) {
+    console.log("Your guess is too low!");
+    return guessNumberGame(attempt + 1);
+  } else {
+    console.log("You won!");
+  }
+};
+
+guessNumberGame();
